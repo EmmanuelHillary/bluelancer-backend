338f67445e14d5de61741a24d3d02bc3ec0818a2
oceanencrypt <emmanuelhillaryugwu@gmail.com>
2023-08-20 21:33:07 +0100

    phone Token

--- src/users/dto/changePassword.dto.ts
+++ src/users/dto/changePassword.dto.ts
@@ -1,28 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
-    ArrayNotEmpty,
-    IsOptional,
-    IsEmail,
-    IsMongoId,
-    Matches,
-    IsDefined,
-    IsString,
-    IsNotEmpty,
-    equals
-  } from 'class-validator';
-  
-  
-  export class ChangePasswordDto {
-      @IsNotEmpty()
-      @IsString()
-      password: string;
-  
-      @IsNotEmpty()
-      @IsString()
-      new_password: string;
-  
-  
-      @IsNotEmpty()
-      @IsString()
-      reEnterNewpassword: string;
-  
-  }
+  ArrayNotEmpty,
+  IsOptional,
+  IsEmail,
+  IsMongoId,
+  Matches,
+  IsDefined,
+  IsString,
+  IsNotEmpty,
+  equals,
+} from 'class-validator';
+
+export class ChangePasswordDto {
+
+  @ApiProperty({ required:true, default:"testpassword1#" })
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty({ required:true, default:"newPassword1#" })
+  @IsNotEmpty()
+  @IsString()
+  new_password: string;
+
+  @ApiProperty({ required:true, default:"newPassword1#" })
+  @IsNotEmpty()
+  @IsString()
+  reEnterNewpassword: string;
+}
